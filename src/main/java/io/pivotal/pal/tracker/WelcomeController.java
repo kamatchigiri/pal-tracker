9b7dbfa2d63f7bbbcd67b062d9c24219866a2182
kamatchigiri <techTrialKam.gmail.com>
2017-11-01 17:27:58 +0530

    Commit ater adding ci files to git

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,27 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+/*    @Value("${WELCOME_MESSAGE}")*/
+    String hello;
+
+
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String msg) {
+        hello = msg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+
+        return hello;
     }
 }
 
--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,5 +1,6 @@
 package test.pivotal.pal.tracker;
 
+import io.pivotal.pal.tracker.EnvController;
 import org.junit.Test;
 
 import java.util.Map;
