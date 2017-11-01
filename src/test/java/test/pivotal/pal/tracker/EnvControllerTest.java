f7ce853003f9b9613388286178a620306d16d5b0
kamatchigiri <techTrialKam.gmail.com>
2017-11-01 17:19:12 +0530

    commit with CI

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -3,7 +3,6 @@ package test.pivotal.pal.tracker;
 import org.junit.Test;
 
 import java.util.Map;
-import io.pivotal.pal.tracker.EnvController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
